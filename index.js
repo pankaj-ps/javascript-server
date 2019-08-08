6f38dc720af93d19c066d7048c28f023168f2499
Pankaj Sharma <pankaj.sharma@lapss66.successivetech.com>
2019-08-08 16:43:27 +0530

    added index file

---
+++ index.js
@@ -0,0 +1,17 @@
+// var express= require('express');
+// var app=express();
+// var port=8000;
+// app.listen(port,function(err){
+//     if(err){
+//         console.log('Oops error in server');
+//     }
+//     else{
+
+// console.log('server started !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+//     }
+// });
+
+// //console.log(typeof(app));
+
+import name from './src/index';
+console.log(name);
